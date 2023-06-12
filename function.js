9f5f2c51012e5596784bcb8d5bd866d9b9cfbd82
Fazil-hello <135647539+Fazil-hello@users.noreply.github.com>
2023-06-12 08:09:50 +0530

    Add files via upload

---
+++ function.js
@@ -0,0 +1,17 @@
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+$("a[href^='#']").click(function(e) {
+	e.preventDefault();
+	
+	var position = $($(this).attr("href")).offset().top;
+
+	$("body, html").animate({
+		scrollTop: position
+	} /* speed */ );
+});
